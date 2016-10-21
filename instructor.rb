b08cd527b5e04c433f22e84ba151a39f488c1a80
Eric Hartman <imerichartman@gmail.com>
2016-10-21 13:22:44 -0400

    Make methods in parent class

--- instructor.rb
+++ instructor.rb
@@ -2,4 +2,8 @@ require_relative 'people'
 
 class Instructor < Item
 
+  def teach
+    puts "Everything in Ruby is an Object"
+  end
+
 end
--- student.rb
+++ student.rb
@@ -1,5 +1,7 @@
 require_relative 'people'
 
 class Student < Item
-
+  def learn
+    puts "I get it!"
+  end
 end
