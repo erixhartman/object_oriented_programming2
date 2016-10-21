1ae65884abc0257fb6dc010f9a8eb0791d234ffa
Eric Hartman <imerichartman@gmail.com>
2016-10-21 13:30:27 -0400

    Exercise 1 complete

--- main.rb
+++ main.rb
@@ -4,5 +4,11 @@ require_relative 'instructor'
 chris     = Instructor.new("Chris")
 cristina  = Student.new("Cristina")
 
-chris.greeting
-cristina.greeting
+# chris.greeting
+# cristina.greeting
+chris.teach
+cristina.learn
+cristina.teach
+chris.learn
+# above 2 won't work because there is no method in their
+# class that matches the command
