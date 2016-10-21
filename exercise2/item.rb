989cc6e6a90323b9e0a418b185daf245ffb58ae3
Eric Hartman <imerichartman@gmail.com>
2016-10-21 14:52:36 -0400

    Attempt 1 of setting taxes and total prices

--- exercise2/item.rb
+++ exercise2/item.rb
@@ -1,35 +1,42 @@
 class Item
-  def initialize(cost, exempt, import)
+  attr_accessor :name
+  attr_reader :exempt, :cost, :import
+
+  def initialize(name, cost, exempt, import)
+  @name   = name
   @cost   = cost
   @exempt = exempt
   @import = import
-
+#  @total_cost = cost * tax
 end
-
-class Exempt < Item
 
   def exempt
+
+
     if true
-      cost = cost
+      sales_tax  = 0
     else
-      cost = cost * 1.1
+      sales_tax  = @cost * 0.1
     end
+
   end
-
-end
-
-end
-
-class Import < Item
 
   def import
+    total_cost = @cost + import_tax
     if true
-      cost = cost
+      import_tax  = @cost * 0.05
     else
-      cost = cost * 1.05
+      import_tax  = 0
     end
+
   end
+
+total_cost = @cost + sales_tax + import_tax
+total_tax  = sales_tax + import_tax
 
 end
 
-x = Item.new(17.99, true, false)
+x = Item.new("book", 17.99, true, false)
+
+#make method for total cost based on
+#behaviour of item is to give total_cost
--- exercise2/receipt.rb
+++ exercise2/receipt.rb
@@ -2,5 +2,9 @@ require_relative 'item'
 
 
 class Receipt
+  puts "1 #{name} at $#{cost}"
 
 end
+
+# Instance variable = [items]
+# method to add to array (<<)
